a978b2a8fc9eed1d09032c20d3eebf314d187dac
men <men.malala@gmail.com>
2023-07-10 09:21:55 +0200

    Update base.sql

--- ci306/base.sql
+++ ci306/base.sql
@@ -8,3 +8,10 @@ create table user
     mail varchar(100),
     mdp varchar(100)
 );
+CREATE TABLE  CODE_ARGENT
+(
+    ID_ARGENT INT PRIMARY KEY AUTO_INCREMENT ,
+    CODE VARCHAR ,
+    VALEUR FLOAT
+);
+INSERT INTO CODE_ARGENT (ID_ARGENT,CODE,VALEUR) VALUES ();
