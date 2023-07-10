577df2af6c6d527b6d2124a73be02273b98d1362
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2023-07-10 15:53:59 +0300

    error deubegage

--- ci306/application/models/Modelinsert_paiement.php
+++ ci306/application/models/Modelinsert_paiement.php
@@ -3,10 +3,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Modelinsert_paiement extends CI_Model {
 
-        $idEtat = 1;
+        //$idEtat = 1;
         public function InsertPaiement($id_argent)
         {
-            $sql="insert into Paiement(ID_ARGENT) values(".$IdUser.")";
+            $sql="insert into Paiement(ID_PAIEMENT,ID_USER,ID_ARGENT) values($id_argent)";
             $query=$this->db->query($sql);
         }
 }
