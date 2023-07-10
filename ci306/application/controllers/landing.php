405572833d50632c1a8cb6e45a986c9af976b0d4
projetETU <120638626+christinaRas@users.noreply.github.com>
2023-07-10 15:59:48 +0300

    suite landing 3

--- ci306/application/controllers/landing.php
+++ ci306/application/controllers/landing.php
@@ -1,26 +1,14 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 class Landing extends MY_Controller 
 {
 
     public function index()
-	{
-		$this->vue('diet');
-	}
-
-	public function genre()
-	{
+    {
         $this->load->model('Model_diet');
+        $data = array();
         $data['genres'] = $this->Model_diet->getGenre();
-        $this->vue('diet',$data);
-	}
-	
-	public function choix()
-	{
-        $this->load->model('Model_diet');
         $data['choix'] = $this->Model_diet->getChoixdiet();
-        $this->vue('diet',$data);
-	}
-   
+        $this->load->view('diet', $data);
+    }
 }
