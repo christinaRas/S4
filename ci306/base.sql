f9cccf4e3d1df1cba87fcb2a1e6c1239e1cc68fb
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2023-07-10 10:58:35 +0300

    modification et ajout donnees à la base

--- ci306/base.sql
+++ ci306/base.sql
@@ -1,6 +1,11 @@
 create database regime;
-use regime;
 
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+use regime;
+--Front Office =================
 create table user
 (
     id_user int primary key AUTO_INCREMENT,
@@ -8,6 +13,50 @@ create table user
     mail varchar(100),
     mdp varchar(100)
 );
+
+create table Description(
+    idDescription int primary key auto_increment not null,
+    id_user int not null;
+    genre varchar(60),
+    taille double,
+    poids double,
+    FOREIGN KEY (id_user) REFERENCES User(id_user)
+)engine=innodb;
+
+create table ChoixObjectif(
+    idCO int primary key auto_increment not null,
+    id_user int not null,
+    idDecription int not null,
+    choix varchar(60),
+    FOREIGN KEY (id_user) REFERENCES User(id_user),
+    FOREIGN KEY (idDescription) REFERENCES Description(idDescription)
+)engine=innodb;
+
+create table regimeMampitombo(
+    idMampitombo int primary key auto_increment not null,
+    titre varchar (100),
+    imageMampitombo varchar(100)
+)engine=innodb;
+
+create table regimeMampihena(
+    idMampihena int primary key auto_increment not null,
+    titre varchar (100),
+    imageMampihena varchar(100)
+)engine=innodb;
+
+create table ActiviteSport(
+    idAS int primary key auto_increment not null,
+    typeSport varchar(100)
+)engine=innodb;
+
+--Back Office ==================================
+
+create table Evolution(
+    idE int primary key auto_increment not null,
+    evolution TIME
+)engine=innodb;
+
+
 
 CREATE VIEW V_NOMBRECLIENT AS SELECT COUNT(*) FROM USER;
 CREATE TABLE  CODE_ARGENT
@@ -28,3 +77,7 @@ CREATE TABLE PAIEMENT
     FOREIGN KEY (ID_USER) REFERENCES USER(ID_USER),
     FOREIGN KEY (ID_ARGENT) REFERENCES CODE_ARGENT(ID_ARGENT)
 );
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
