09f1c7be820bda228619e97485ea891174433274
men <men.malala@gmail.com>
2023-07-10 14:00:07 +0200

    integration BackOffice1

--- ci306/application/views/layout/Header.php
+++ ci306/application/views/layout/Header.php
@@ -22,21 +22,22 @@ $title = basename($url);
   <meta content="" name="description">
   <meta content="" name="keywords">
   <!-- Favicons -->
-  <link href="temp/<?php  echo base_url('img/favicon.png'); ?>" rel="icon">
-  <link href="temp/<?php  echo base_url('img/apple-touch-icon.png'); ?>" rel="apple-touch-icon">
+  <link href="<?php  echo base_url('temp/img/favicon.png'); ?>" rel="icon">
+  <link href="<?php  echo base_url('temp/img/apple-touch-icon.png'); ?>" rel="apple-touch-icon">
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
   <!-- Vendor CSS Files -->
-  <link href="temp/<?php   echo base_url('vendor/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
-  <link href="temp/<?php   echo base_url('vendor/bootstrap-icons/bootstrap-icons.css');?>" rel="stylesheet">
-  <link href="temp/<?php   echo base_url('vendor/boxicons/css/boxicons.min.css');?>" rel="stylesheet">
-  <link href="temp/<?php   echo base_url('vendor/quill/quill.snow.css');?>temp" rel="stylesheet">
-  <link href="temp/<?php   echo base_url('vendor/quill/quill.bubble.css');?>" rel="stylesheet">
-  <link href="temp/<?php   echo base_url('vendor/remixicon/remixicon.css');?>" rel="stylesheet">
-  <link href="temp/<?php   echo base_url('vendor/simple-datatables/style.css');?>" rel="stylesheet">
+  <link href="<?php   echo base_url('temp/vendor/bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
+  <link href="<?php   echo base_url('temp/vendor/bootstrap-icons/bootstrap-icons.css');?>" rel="stylesheet">
+  <link href="<?php   echo base_url('temp/vendor/boxicons/css/boxicons.min.css');?>" rel="stylesheet">
+  <link href="<?php   echo base_url('temp/vendor/quill/quill.snow.css');?>temp" rel="stylesheet">
+  <link href="<?php   echo base_url('temp/vendor/quill/quill.bubble.css');?>" rel="stylesheet">
+  <link href="<?php   echo base_url('temp/vendor/remixicon/remixicon.css');?>" rel="stylesheet">
+  <link href="<?php   echo base_url('temp/vendor/simple-datatables/style.css');?>" rel="stylesheet">
   <!-- Template Main CSS File -->
+  <link href="<?php echo base_url('temp/css/style.css');?>" rel="stylesheet">
   <link href="<?php echo base_url('css/style.css');?>" rel="stylesheet">
 
   <style>
