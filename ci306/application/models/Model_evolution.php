b03cd96797c94102a00a2b95ce5259bbd4d76b81
projetETU <120638626+christinaRas@users.noreply.github.com>
2023-07-11 02:03:02 +0300

    update

--- ci306/application/models/Model_evolution.php
+++ ci306/application/models/Model_evolution.php
@@ -15,4 +15,10 @@ class Model_evolution extends CI_Model {
         $sql="insert into Evolution values(null,".$id_user.",".$newpoids.",'".$date."')";
         $query=$this->db->query($sql);
     }
+    public function returne($id_user)
+    {
+        $sql="select id_choix from description where id_user=".$id_user." ";
+        $query=$this->db->query($sql);
+        return $query->result();
+    }
 }
