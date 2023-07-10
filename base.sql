63a41b3ee1f0d266d4e8111052b6d856008e2d21
projetETU <120638626+christinaRas@users.noreply.github.com>
2023-07-10 10:04:29 +0300

    base

---
+++ base.sql
@@ -0,0 +1,10 @@
+create database regime;
+use regime;
+
+create table user
+(
+    id_user int,
+    nom_user varchar(100),
+    mail varchar(100),
+    mdp varchar(100)
+);
--- ci306/application/models/Model_login.php
+++ ci306/application/models/Model_login.php
@@ -2,26 +2,21 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_login extends CI_Model {
-    
+
     public function checkLogin($mail,$pass)
         {
             $valiny = false;
-            $sql = "select * from User where email= '".$mail."' and mdp= '".$pass."' ";
+            $sql = "select * from user where mail= '".$mail."' and mdp= '".$pass."' ";
             $query = $this->db->query($sql);
             foreach($query->result_array() as $row)
             {
-                if($mail == $row['email']  && $pass == $row['mdp'])
+                if($mail == $row['mail']  && $pass == $row['mdp'])
                 {
                     
                     $valiny = true;
                     $this->load->library('session');
-                    $this->session->set_userdata('id', $row['idUser']);
-                    $this->session->set_userdata('admin', $row['identification']);
-                    $this->session->set_userdata('nom', $row['nom']);
-                    $this->session->set_userdata('pdp', $row['photo']);
-                    $this->session->set_userdata('admin', $row['identification']);
-
-
+                    $this->session->set_userdata('id', $row['id_user']);
+                    $this->session->set_userdata('nom', $row['nom_user']);
                 }
             }
             return $valiny;
@@ -29,8 +24,7 @@ class Model_login extends CI_Model {
         public function inscri($email,$mdp,$nom)
         {
             $photo='assets/img/men.jpg';
-            $sql="insert into User values(null,'".$email."','".$mdp."','".$nom."',0,'".$photo."')";
+            $sql="insert into user values(null,'".$email."','".$mdp."','".$nom."')";
             $query=$this->db->query($sql);
         }
-
 }
