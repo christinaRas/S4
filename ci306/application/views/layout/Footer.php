65fc6b3c76dd7fb7c2e45300f6763e73e80906d4
men <men.malala@gmail.com>
2023-07-10 10:28:18 +0200

    link css template

--- ci306/application/views/layout/Footer.php
+++ ci306/application/views/layout/Footer.php
@@ -4,3 +4,15 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.min.js"></script>
 </script>
 </footer>
+<script src="<?php echo base_url('vendor/apexcharts/apexcharts.min.js');?>/temp"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/chart.js/chart.umd.js"></script>
+  <script src="vendor/echarts/echarts.min.js"></script>
+  <script src="vendor/quill/quill.min.js"></script>
+  <script src="vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="vendor/tinymce/tinymce.min.js"></script>
+  <script src="vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="js/main.js"></script>
+<?php echo base_url('') ?>
