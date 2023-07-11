cc228f7a1730597abebaec21b20bf75519c5e6c2
projetETU <120638626+christinaRas@users.noreply.github.com>
2023-07-11 07:58:42 +0300

    update

--- ci306/application/controllers/Evolutionuser.php
+++ ci306/application/controllers/Evolutionuser.php
@@ -14,16 +14,10 @@ class Evolutionuser extends MY_Controller
         $id_user = $this->session->userdata('id_user');
         $newpoids = $this->input->post("newpoids");
         $date = $this->input->post("date");
-
+    
         $this->load->model('Model_evolution');
-        if($this->Model_evolution->saveEvolution($id_user, $newpoids, $date))
-        {
-            $choix = $this->Model_evolution->returne($id_user);
-            if ($choix == 1) {
-                redirect('../Statistique/stat'); // Redirection vers le contrôleur "Augmentation" et la méthode "plat"
-            } elseif ($choix == 2) {
-                redirect('../Diminution/plat'); // Redirection vers le contrôleur "Diminution" et la méthode "plat"
-            }
-        }
+        $choix = $this->Model_evolution->returne($id_user);
+        $this->Model_evolution->saveEvolution($id_user, $newpoids, $date);
+        header("Location: " . $base_url . '../Statistique/stat');
     }
 }
