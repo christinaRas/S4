ab15346c371324eb230a8b6fe79bfeae7212564c
men <men.malala@gmail.com>
2023-07-11 06:37:22 +0200

    update carte credit

--- ci306/application/controllers/Wallet.php
+++ ci306/application/controllers/Wallet.php
@@ -11,12 +11,31 @@ class Wallet extends MY_Controller
        $data['result'] = $this->WalletModel->getWalletUser($id);
         $this->vue('Wallet',$data);
     }
+    public function error()
+    {
+        $this->vue("error");
+    }
     public function codeValid()
     {
         $id = $this->session->userdata('id_user');
         $this->load->model("WalletModel");
         $data = array();
         $data['result'] = $this->WalletModel->getcode();
-
+        $this->vue("ListeCredit",$data);
+    }
+    public function insertCode()
+    {
+        $data = array(
+            'id_paiement' => null,
+            'id_user' => $this->session->userdata('id_user') ,
+            'id_argent' => $this->input->post("code") ,
+            'etat' => 1
+        );
+        if ($this->db->insert("paiement",$data))
+        {
+            $this->index();
+        }else {
+            $this->error();
+        }
     }
 }
---
+++ ci306/application/views/ListeCredit.php
@@ -0,0 +1,12 @@
+<form class="bg-dark text-light p-4" method="post" action="<?php echo base_url('..'); ?>">
+  <h2 class="text-light mb-4">Choisir crédit</h2>
+  <div class="form-group">
+    <label for="selectOption" class="form-label">Option :</label>
+    <select class="form-control" name="code" id="selectOption">
+        <?php  foreach ($result as $results) { ?>
+      <option value="<?php echo $results->id_argent ?>"><?php echo $results->code ?></option>
+    <?php } ?>
+    </select>
+  </div>
+  <button type="submit" class="btn btn-primary">Valider</button>
+</form>
