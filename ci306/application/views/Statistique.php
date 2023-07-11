ced7b489e467de81501d5250811f75088e4095da
Lee-Rudy <120602756+Lee-Rudy@users.noreply.github.com>
2023-07-11 08:18:01 +0300

    redirection

--- ci306/application/views/Statistique.php
+++ ci306/application/views/Statistique.php
@@ -8,9 +8,9 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
 
     <div class="d-flex align-items-center justify-content-between">
-    <a href="#" class="logo d-flex align-items-center">
+    <a href="<?php echo base_url('../Augmentation/plat'); ?>" class="logo d-flex align-items-center">
         <img src="assets/img/logo.png" alt="">
-        <span class="d-none d-lg-block">BackOffice</span>
+        <span class="d-none d-lg-block">suivi & evolution-retour à l'acceuil</span>
     </a>
     </nav><!-- End Icons Navigation -->
 
